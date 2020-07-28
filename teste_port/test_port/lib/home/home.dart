bb525f11a42686a5e23d0b1999bc0403cd332936
Kleberson Toro <klebinho.tvf@gmail.com>
2020-07-28 18:08:26 -0300

    first commit

---
+++ teste_port/test_port/lib/home/home.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    //? Feed, e rotas para as outras partes do app
+    return Scaffold(
+      drawer: Drawer(), //? Widget da "gaveta" posicionada a direita
+      appBar: AppBar(
+        title: Text("Nome do App"),
+        centerTitle: true,
+      ),
+      body: Column(
+        //crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              //? Icones de rota
+              IconButton(
+                icon: Icon(Icons.library_books),
+                iconSize: 50,
+                color: Theme.of(context).primaryColor,
+                onPressed: (){
+                  //? Script para realizar rota
+
+                },
+              ),
+              SizedBox(width: 30,),
+              IconButton(
+                icon: Icon(Icons.gamepad),
+                iconSize: 50,
+                color: Theme.of(context).primaryColor,
+                onPressed: (){
+                  //? Script para realizar rota
+
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ teste_port/test_port/lib/lessons/lessons.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LessonsScreen extends StatefulWidget {
+  LessonsScreen({Key key}) : super(key: key);
+
+  @override
+  _LessonsScreenState createState() => _LessonsScreenState();
+}
+
+class _LessonsScreenState extends State<LessonsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    //? Lições e atividades
+    return Scaffold();
+  }
+}

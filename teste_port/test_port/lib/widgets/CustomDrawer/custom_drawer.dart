f7e84178f9c771109a363e6dac73888212439371
Kleberson Toro <klebinho.tvf@gmail.com>
2020-07-28 21:40:18 -0300

    Drawer Update II

--- teste_port/test_port/lib/widgets/CustomDrawer/custom_drawer.dart
+++ teste_port/test_port/lib/widgets/CustomDrawer/custom_drawer.dart
@@ -1,8 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:test_port/widgets/CustomDrawer/custom_drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          /* Container(
+            //color: Theme.of(context).primaryColor,
+            height: 80,
+            margin: EdgeInsets.all(15),
+            child: logoSuspensom,
+          ), */
+          //MainDrawerHeader(),
+          Container(
+            margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+            child: Column(
+              children: <Widget>[
+                  Divider(),
+
+                  DrawerTile(
+                    title: "Lições",
+                    iconData: Icons.note,
+                    page: 1,
+                  ),
+                  
+                  DrawerTile(
+                    title: "Mensagens",
+                    iconData: Icons.chat_bubble,
+                    page: 2,
+                  ),
+
+              ],
+            ),
+          )
+
+        ],
+      ),
+    );
   }
 }

644b7ed865741298485d2491676213cd6fb17053
Kleberson Toro <klebinho.tvf@gmail.com>
2020-07-28 21:45:38 -0300

    Main MultiProvider

--- teste_port/test_port/lib/main.dart
+++ teste_port/test_port/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:test_port/models/page_manager.dart';
 import 'package:test_port/screens/home/home_screen.dart';
 
 void main() {
@@ -9,14 +11,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App Port', //teste III
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    final PageController pageController = PageController();
+
+    return MultiProvider(
+      providers: [
+        Provider(
+          create: (_) => PageManager(pageController),
+        )
+      ],
+      child: MaterialApp(
+        title: 'App Port', //teste III
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: HomeScreen(),
       ),
-      home: HomeScreen(),
     );
   }
 }
-
